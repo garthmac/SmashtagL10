35bd1c4e928c2ac7ed63f533a99071e3b0cc427c
iMac21.5 <imac@iMac215.local>
2015-04-29 09:41:26 -0600

    Initial Commit

---
+++ SmashtagL10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmashtagL10
+//
+//  Created by iMac21.5 on 4/29/15.
+//  Copyright (c) 2015 Garth MacKenzie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
